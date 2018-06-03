aab1c3e23fc81cd86c0be3bf0c80b56415fd7f0f
Tom Rowley <vulcan@rowley.com>
2018-06-03 09:48:18 -0700

    release

---
+++ Pre-Release/J5appV2.1.js
@@ -0,0 +1,269 @@
+'use strict';
+
+var five = require('johnny-five'),board,motor,button;
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+const SerialPort = require('serialport');
+const Readline = SerialPort.parsers.Readline;
+const MFMport = new SerialPort('COM11',{
+  baudRate: 19200             });
+const parser = new Readline({delimiter: '\r'});
+MFMport.pipe(parser);
+
+
+
+let led = null;
+
+
+let gaTime=0,inPwm=0,outPwm=0,rsTime=0,rpPwm =0;
+let sData ={}; 
+
+ /***** define Pins ********/ 
+// PWM pins are 3, 5, 6, 9, 10, and 11 but avoid using Pin 3 
+//Only PWM pins can be used for Motor objects
+// Sensor inputs
+let wsPin = 2;
+let sssrPin = 3;
+let tempPin = 4;// Dallas temperature sensor
+
+// Actuator pins
+let gaPin =5;// Ga inject
+let rpPin = 6;// recovery motor
+//let ?? = 7;// 
+//let uknPin = 8;// 
+let inPin = 9;// water IN
+let outPin = 10;// water OUT
+let rsPin = 11; // solenoid
+//let ?? = 12;// 
+
+
+
+let offState = true;// state of ws sensor 
+let motorOff = false; // INmotor can run initially; flag set by water sensor
+let GRBoff = true; // flag set by SSSR
+let GRBenable = false;
+
+
+
+app.use(express.static(__dirname + '/public'));
+app.get('/', function(req, res, next) {
+  res.sendFile(__dirname + '/demoIndexV1.1.html')
+});
+
+//board = new five.Board();
+var board = new five.Board({
+  port: "COM9",
+  repl: false  
+});
+board.on("ready", function() {
+  console.log('Arduino is ready.');
+ 
+// actuators
+  var gaMotor = new five.Motor({
+    pin: gaPin
+  }); 
+  var rsMotor = new five.Motor({
+    pin: rsPin
+  });
+  var inMotor = new five.Motor({
+    pin: inPin
+  });
+  var outMotor = new five.Motor({
+    pin: outPin
+  });
+  var rpMotor = new five.Motor({
+    pin: rpPin
+  });  
+    
+// switches
+  var ws = new five.Button(wsPin); 
+  var sssr = new five.Button(sssrPin); 
+    
+ // water IN pump
+if(GRBoff){
+ ws.on("down", function() {
+   motorOff = false;
+   inMotor.start(inPwm);
+   sData = {
+       sensor: "ws",
+       value: 1
+            };
+    io.emit('sensor', sData);   
+    console.log( "Button down" );
+  }).on("up", function() {
+    motorOff =true;
+    inMotor.stop();
+    sData = {
+       sensor: "ws",
+       value: 0
+            };
+    io.emit('sensor', sData);  
+    console.log( "Button up" );
+  });// end water IN pump
+}// end of GRB test for inMotor     
+
+  
+   // ga Recovery pump   
+ sssr.on("down", function() {// 'Down' means there is a contact with Ga
+   sData = {
+       sensor: "GRBs",
+       value: 1
+            };
+   io.emit('sensor', sData);   
+   console.log( "Ga present" );  
+    
+   GRBoff = false;
+   rpMotor.start(255);
+
+   console.log( "GRBoff false - turn off water motors"); 
+   inMotor.stop();
+   outMotor.stop(); 
+ });    
+ sssr.on("up", function() {
+    GRBoff =true;
+    rpMotor.stop();
+    sData = {
+       sensor: "GRBs",
+       value: 0
+            };
+    io.emit('sensor', sData);  
+    console.log( "No Ga sensed" );
+    console.log( "GRBoff true - turn ON water motors");
+      outMotor.start(outPwm);
+      if(motorOff){
+          inMotor.stop();
+      }else{
+          inMotor.start(inPwm);
+      };
+     
+  });// end GA recovery pump
+
+    
+  // Listen to the web socket connection
+  io.on('connection', function(client) {
+
+  client.on('motor', function(data) {
+    console.log( data);
+    switch (data.device) {
+    case "gaMotor":
+        gaTime = data.value*1000;// Set value for trigger 
+        break;
+        
+    case "rsMotor":
+        rsTime = data.value*1000; // Set value for trigger command
+        break; 
+        
+    case "inMotor":
+        inPwm = data.value.toFixed(0);
+        if(GRBoff){    
+         if(motorOff){
+          inMotor.stop();
+         }else {
+           inMotor.start(inPwm);
+         }
+        }
+        break;
+            
+       case "outMotor":
+        outPwm = data.value.toFixed(0); 
+        if(GRBoff){ 
+         outMotor.start(outPwm);
+        }
+        break; 
+              
+        }// end of switch                                 
+   });//end of "motor"
+    
+ client.on('trigger', function(data) {
+    console.log( data);
+    switch (data.device) {
+    case "GAtrigger":
+    // "start" events fire when the motor is started.
+        gaMotor.on("start", function() {
+         console.log("inject start", Date.now());
+
+       // Ga Inject motor stop in gaTime milliseconds
+          board.wait(gaTime, function() {
+            gaMotor.stop();
+            console.log("inject stop", Date.now()); 
+            });
+        });
+        gaMotor.start(255);    
+        break; 
+    case "RStrigger":
+        // "start" events fire when the motor is started.
+        rsMotor.on("start", function() {
+         console.log("Solenoid start", Date.now());
+
+       // Solenoid motor stop in rsTime milliseconds
+          board.wait(rsTime, function() {
+            rsMotor.stop();
+            });
+        });
+        rsMotor.start(255);
+        console.log( "turn off water motors - give SSR sensor time to react"); 
+        inMotor.stop();
+        outMotor.stop();   
+        break; 
+         };   // end of switch  
+    });  //end of trigger
+    
+    
+
+  });// end of websocket loop
+  
+  
+
+   
+    
+});//end of Johnny-five loop
+
+
+parser.on('data', function(data) {
+    //console.log('data: ',data)
+    
+       // parse received string 
+      // data = device ID('A'), absolute pressure, temperature, volumetric flow rate,
+      // mass flow rate, setpoint value, and selected gas
+      // split out  and Only forward 'mass flow rate'
+     var mfmData = data.toString();
+          //console.log('MFM String: '+ data);
+      var parts = mfmData.split(" ");
+      var mf =parseFloat(parts[4]);
+      var pressure =parseFloat(parts[1]);
+      var gasTemp = parseFloat(parts[2]);
+    
+     /*send mfm gasTemp value to UI
+      var tempData = {
+           sensor: "temp",
+           value: gasTemp
+                };
+       io.emit('sensor', tempData);
+        console.log("gastemp: " + gasTemp);
+        */ 
+    
+      // send mfm value to UI
+       var mData = {
+           sensor: "mfm",
+           value: mf
+                };
+       io.emit('sensor', mData);
+        console.log("MFM: " + mf);        
+  
+    });  //end of parsers setup
+    
+setInterval( MFMpoll , 100);
+
+function MFMpoll(){
+    MFMport.write('A\r');         //console.log('Sending A');  
+ }
+
+
+const port = process.env.PORT || 3000;
+
+server.listen(port);
+console.log(`Server listening on http://localhost:${port}`);
+
---
+++ Pre-Release/public/siclient.js
@@ -0,0 +1,247 @@
+
+function externalJS(){ // this is to cause immediate loading of js file 
+ // IMPORTANT - ALL ElementByIds MUST BE DEFINED OR
+// JS DOESN"T WORK
+    
+//    var l = document.getElementById('l');
+var log = function (m) {
+    var i = document.createElement('li');
+    i.innerText = new Date().toISOString()+' '+m;
+//    l.appendChild(i);
+}
+log('opening socket.io connection');
+var s = io();
+s.on('connect_error', function (m) { log("error"); });
+s.on('connect', function (m) { log("socket.io connection open"); });
+s.on('message', function (m) { log(m); });
+//s.on('sensor', function (m) { log(m.value); });
+    
+
+    
+ // debug frame  
+ var debugDiv = document.getElementById("debug"); 
+debugDiv.innerHTML ='TEST';    
+    
+ var inPWM =0;           
+var outPWM =0;
+var rpPWM =0;
+var temperature = 55;
+ 
+ // -------------Actuators --------------
+ 
+   //set GA inject time
+var gaTime = document.getElementById('gaTime'),
+    gaTimeDiv = document.getElementById("gaAmt");
+    
+gaTime.oninput = function() {
+       v = this.value;
+       gaAmt.value = v;
+       //gaTimeDiv.innerHTML = v;
+       s.emit('motor', {
+            device: "gaMotor",
+            value: v
+        }); 
+    }
+ gaAmt.onchange = function() {
+       v = this.value;
+       gaAmt.value = v;
+       gaTime.value =v;
+       //gaTimeDiv.innerHTML = v;
+       s.emit('motor', {
+            device: "gaMotor",
+            value: v
+        }); 
+    }   
+
+     // Set solonoid valve release timing
+var rsTime = document.getElementById('rsTime'),
+    rsTimeDiv = document.getElementById("rsAmt");
+    
+rsTime.oninput = function() {
+       v = this.value;
+       rsAmt.value = v;
+       //gaTimeDiv.innerHTML = v;
+       s.emit('motor', {
+            device: "rsMotor",
+            value: v
+        }); 
+    }
+ rsAmt.onchange = function() {
+       v = this.value;
+       rsAmt.value = v;
+       rsTime.value =v;
+       //gaTimeDiv.innerHTML = v;
+       s.emit('motor', {
+            device: "rsMotor",
+            value: v
+        }); 
+    } 
+ 
+ // set water IN pump rate 
+var INTime = document.getElementById('INTime');
+ var inTimeDiv = document.getElementById("inAmt");
+      
+
+INTime.onchange = function() {
+       inPWM = this.value;
+       inTimeDiv.innerHTML = this.value;
+       s.emit('motor', {
+            device: "inMotor",
+            value: inPWM
+        }); 
+    }
+
+// Set water OUT pump rate 
+var OUT = document.getElementById('OUT'),
+    outDiv = document.getElementById("outAmt");
+
+OUT.onchange = function() {
+       outPWM = this.value;
+       outDiv.innerHTML = this.value;
+       s.emit('motor', {
+            device: "outMotor",
+            value: outPWM
+        }); 
+    }
+
+// send GA trigger message
+var tr =  document.getElementById("tr");
+
+tr.onclick = function() {
+       s.emit('trigger', {
+            device: "GAtrigger",
+            value: 1
+        }); 
+    }
+
+// send solenoid trigger message
+var rs =  document.getElementById("rs");
+
+rs.onclick = function() {
+       s.emit('trigger', {
+            device: "RStrigger",
+            value: 1
+        }); 
+    }
+
+// send reclaim pump message
+ var rp = document.getElementById('recPump'),
+    rpDiv = document.getElementById("rpAmt");
+
+rp.onchange = function() {
+       rpPWM = this.value;
+       rpDiv.innerHTML = this.value;
+       s.emit('motor', {
+            device: "rpMotor",
+            value: rpPWM
+        }); 
+    }   
+ 
+//------- config  --------------
+
+ var dropDown = document.getElementById("defaultNumbers");
+
+
+//var gaPin = document.getElementById('gaPin'),
+   // gaPinDiv = document.getElementById("gaAmt");
+
+gaPin.onchange = function() {
+    dropDown.selectedIndex = 0;
+       v = this.value;
+       s.emit('config', {
+            device: "gaPin",
+            pin: v
+        }); 
+    }
+
+inPin.onchange = function() {
+       v = this.value;
+       s.emit('config', {
+            device: "inPin",
+            pin: v
+        }); 
+    }
+
+outPin.onchange = function() {
+       v = this.value;
+       s.emit('config', {
+            device: "outPin",
+            pin: v
+        }); 
+    }
+
+recPin.onchange = function() {
+       v = this.value;
+       s.emit('config', {
+            device: "recPin",
+            pin: v
+        }); 
+    }
+
+ s1Pin.onchange = function() {
+       v = this.value;
+       s.emit('config', {
+            device: "rsPin",
+            pin: v
+        }); 
+    }
+
+wsPin.onchange = function() {
+       v = this.value;
+       s.emit('config', {
+            device: "wsPin",
+            pin: v
+        }); 
+    }
+
+tofPin.onchange = function() {
+       v = this.value;
+       s.emit('config', {
+            device: "tofPin",
+            pin: v
+        }); 
+    }
+
+ tempPin.onchange = function() {
+       v = this.value;
+       s.emit('config', {
+            device: "tempPin",
+            pin: v
+        }); 
+    }
+ 
+
+
+//--------------Sensors -----------------
+   var GAtemp = document.getElementById("GART");
+    
+        s.on('sensor', function(data) {
+        
+        switch(data.sensor){
+               
+         case "temp":     
+            temperature = data.value;
+            GAtemp.innerHTML = temperature  + ' C degrees';
+            tempData.setValue(0, 1, temperature);
+            tempChart.draw(data, options);    
+            break;
+            
+            case "ws":
+             if(data.value == 0){
+                debugDiv.innerHTML ='ON';
+                waterSensorOn() ;
+              }else if (data.value == 1) {
+                 debugDiv.innerHTML ='OFF';
+                 waterSensorOff();
+              }
+             break;
+               
+            }//end of switch
+               
+    }); // end of sensor processing
+      
+
+ }// end of loading function 
+
+
+
